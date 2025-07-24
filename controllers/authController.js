566291de1546d35a4df934ebc76ddae0f2285d1b
tintrung <huynhtrungtin1506@gmail.com>
2025-07-24 12:32:41 +0700

    update env

--- controllers/authController.js
+++ controllers/authController.js
@@ -60,7 +60,6 @@ exports.login = async (req, res) => {
         id: user._id,
         email: user.email,
         username: user.username,
-        photoUrl: user.photoUrl,
         role: user.role,
         favoritesProduct: user.favoritesProduct,
         favoritesPost: user.favoritesPost,
